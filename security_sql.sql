36eced6aeb7dcc621ab090f26883b5fa613f747f
qiquinn <ccc6463@outlook.com>
2019-07-04 10:28:44 +0800

    插入数据出错BUG解决2

--- security_sql.sql
+++ security_sql.sql
@@ -46,6 +46,23 @@ CREATE TABLE `security_colum` (
   PRIMARY KEY (`colunm_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
+/** 创建文章表 **/
+DROP TABLE IF EXISTS `security_article`;
+CREATE TABLE `security_article` (
+  `id` smallint(6) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `content` longtext DEFAULT NULL,
+  `ishander` tinyint(1) DEFAULT NULL,
+  `culomn_id` int DEFAULT NULL,
+  `create_date` varchar(25) not null,
+  `modify_date` varchar(25) not null,
+  `page_views` int not null default 0,
+  PRIMARY KEY (`id`),
+  foreign key (culomn_id) references security_article_type(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
 insert into `security_colum` (colunm_name,column_pid) value ('前台',0);
 insert into `security_colum` (colunm_name,column_pid) value ('后台',0);
 insert into `security_colum` (colunm_name,column_pid) value ('移动端',1);
@@ -64,7 +81,7 @@ insert into `security_article_type` (article_type) value('热点法规');
 insert into `security_article_type` (article_type) value('政策法规');
 insert into `security_article_type` (article_type) value('热点稿件');
 
-insert into security_article (`title`,content,isHander,culomn_id,create_data,modify_date) value('热烈庆祝型中国成立70周年','习大大发表讲话:dsafasfsafsafasdfasfasfasfsafadsf十大发生发撒的肥肉我去饿饿哇
+insert into security_article (`title`,content,ishander,culomn_id,create_date,modify_date) value('热烈庆祝型中国成立70周年','习大大发表讲话:dsafasfsafsafasdfasfasfasfsafadsf十大发生发撒的肥肉我去饿饿哇
 f十大发生发f十大发生发f十大发生发f十大发生发
 f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发
 f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发
@@ -139,9 +156,12 @@ f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发
 f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发f十大发生发
 ',0,3,'2019-03-04','2019-03-21');
 
-insert into security_article (`title`,content,isHander,culomn_id,create_data,modify_date) value('热烈庆祝型中国成立70周年','习大大发表讲话:dsafasfsafsafasdfasfasfasfsafadsf十大发生发撒的肥肉我去饿饿哇',1,3,'2019-03-04','2019-03-21');
-insert into security_article (`title`,content,isHander,culomn_id,create_data,modify_date) value('关于XXX的通知','位于2019:热天耐热然而父亲的而且我发的',0,1,'2019-03-04','2019-03-21');
-insert into security_article (`title`,content,isHander,culomn_id,create_data,modify_date) value('关于Xqy的通知','XQY is QiQiuinn:dsafasfsafsafasdfasfasfasfsafadsf十大发生发撒的肥肉我去饿饿哇',0,1,'2019-05-04','2019-05-21');
-insert into security_article (`title`,content,isHander,culomn_id,create_data,modify_date) value('法律1','法律一',0,2,'2019-03-04','2019-03-21');
-insert into security_article (`title`,content,isHander,culomn_id,create_data,modify_date) value('法律2','法律二:',0,2,'2019-11-04','2019-11-11');
-insert into security_article (`title`,content,isHander,culomn_id,create_data,modify_date) value('法律3','法律三:',0,2,'2019-11-04','2019-11-11');
+insert into security_article (`title`,content,ishander,culomn_id,create_date,modify_date) value('热烈庆祝型中国成立70周年','习大大发表讲话:dsafasfsafsafasdfasfasfasfsafadsf十大发生发撒的肥肉我去饿饿哇',1,3,'2019-03-04','2019-03-21');
+insert into security_article (`title`,content,ishander,culomn_id,create_date,modify_date) value('关于XXX的通知','位于2019:热天耐热然而父亲的而且我发的',0,1,'2019-03-04','2019-03-21');
+insert into security_article (`title`,content,ishander,culomn_id,create_date,modify_date) value('关于Xqy的通知','XQY is QiQiuinn:dsafasfsafsafasdfasfasfasfsafadsf十大发生发撒的肥肉我去饿饿哇',0,1,'2019-05-04','2019-05-21');
+insert into security_article (`title`,content,ishander,culomn_id,create_date,modify_date) value('法律1','法律一',0,2,'2019-03-04','2019-03-21');
+insert into security_article (`title`,content,ishander,culomn_id,create_date,modify_date) value('法律2','法律二:',0,2,'2019-11-04','2019-11-11');
+insert into security_article (`title`,content,ishander,culomn_id,create_date,modify_date) value('法律3','法律三:',0,2,'2019-11-04','2019-11-11');
+insert into security_article (`title`,content,ishander,culomn_id,create_date,modify_date) value('关于XXX的通知','位于2019:热天耐热然而父亲的而且我发的',0,1,'2019-03-04','2019-03-21')	Error Code: 1054. Unknown column 'create_data' in 'field list'	0.000 sec
+
+
