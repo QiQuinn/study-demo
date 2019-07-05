1d1d457539bf4ecf68eff38e069cca7a5aec3079
qiquinn <ccc6463@outlook.com>
2019-07-05 16:29:02 +0800

    更改TureOrFalse

--- utlis/src/main/java/com/qiquinn/security/utils/verdifyparam/customeranotation/TrueOrFalse.java
+++ utlis/src/main/java/com/qiquinn/security/utils/verdifyparam/customeranotation/TrueOrFalse.java
@@ -13,7 +13,7 @@ import java.lang.annotation.Target;
  * Created by QiQuinn on 2019/7/5.
  * 字段验证功能：数字1 0 验证类
  */
-@Target(ElementType.FIELD)  //作用于的类型，此处为对象的属性
+@Target(ElementType.FIELD)  //作用于的类型，此处为对象的属性s
 @Retention(RetentionPolicy.RUNTIME)  //运行时生效
 @Constraint(validatedBy = IsHanderValidtor.class) //通过PasswordNotNullValidator类实现注解的相关校验操作
 public @interface TrueOrFalse {
@@ -24,9 +24,10 @@ public @interface TrueOrFalse {
     Class<?>[] groups() default {};  //必填，下文会将到此参数的作用
     Class<? extends Payload>[] payload() default {}; //必填
 
-    @Target({ElementType.FIELD,ElementType.METHOD,ElementType.TYPE})
-    @interface Hander
-    {
-        TrueOrFalse[] value();
-    }
+//
+//    @Target({ElementType.FIELD,ElementType.METHOD,ElementType.TYPE})
+//    @interface Hander
+//    {
+//        TrueOrFalse[] value();
+//    }
 }
